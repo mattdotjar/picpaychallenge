77e2fcad95deedde475cab8aa1d018c9078a5433
Matt <mattmoura.jvm@gmail.com>
2024-11-08 08:35:58 -0300

    first commit

---
+++ src/test/java/com/picpaychallenge/picpay_challenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaychallenge.picpay_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
