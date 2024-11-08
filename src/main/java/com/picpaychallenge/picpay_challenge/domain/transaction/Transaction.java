bee51cef5a186f3fcceac1b722d9ada9e0330983
matt <mattmoura.jvm@gmail.com>
2024-11-08 09:32:19 -0300

    Created Transaction class/entity

--- src/main/java/com/picpaychallenge/picpay_challenge/domain/transaction/Transaction.java
+++ src/main/java/com/picpaychallenge/picpay_challenge/domain/transaction/Transaction.java
@@ -1,11 +1,36 @@
 package com.picpaychallenge.picpay_challenge.domain.transaction;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.picpaychallenge.picpay_challenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
-
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+    
+
+
+
 }
